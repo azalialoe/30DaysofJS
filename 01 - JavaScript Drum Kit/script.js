fb839fb0227ac4deb4f98f772420de887c6f425a
azalialoe <azalia.loeciano@gmail.com>
2021-11-03 20:23:51 -0400

    Day 1 - finished code & changed some styling

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,18 @@
+const playDrumSound = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+window.addEventListener('keydown', playDrumSound)
+
+const removeTransition = (e) => {
+if(e.propertyName !== 'transform') return;
+e.target.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
