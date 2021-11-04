ce6426fc97ac25eb3243137edab2f10964936649
azalialoe <azalia.loeciano@gmail.com>
2021-11-04 17:41:08 -0400

    finished Day 2, added time display on page

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,34 @@
+const setDate = () => {
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    if (seconds === 0 || mins === 0 || hour === 00 || hour === 24) {
+        secondHand.style.transition = 'all 0s';
+        minuteHand.style.transition = 'all 0s';
+        hourHand.style.transition = 'all 0s';
+    }
+    
+    const time = document.getElementById('time');
+    if (seconds < 10 ) {
+        time.innerHTML = `${hour}:${mins}:0${seconds}`
+    } else {
+        time.innerHTML = `${hour}:${mins}:${seconds}`
+    }
+}
+
+setInterval(setDate, 1000);
+
