8f6f00dc697ff38fccac9ff99f5489fb2330afaa
azalialoe <azalia.loeciano@gmail.com>
2021-11-09 19:23:33 -0500

    project 3 added

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('.controls input');
+
+const handleUpdate = (e) => {
+    
+    const suffix = e.target.dataset.sizing || '';
+
+    document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
