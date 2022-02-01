89561eeedfa07de5781728d783b9c912f1682e77
azalialoe <azalia.loeciano@gmail.com>
2022-01-31 20:08:31 -0500

    added project 19 (not finished)

---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,13 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediateDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+        })
+}
